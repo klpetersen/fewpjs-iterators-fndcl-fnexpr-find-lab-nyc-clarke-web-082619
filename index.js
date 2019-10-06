30e30a1a49909c231fa16617f43571aa95a8c841
github username <github email address>
2019-10-05 20:31:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+    let find_w = array.find(function(s) { return s.result === "W" } ); 
+      if (find_w !== undefined){ 
+        return find_w.year 
+      }
+      else {
+  return find_w 
+}
 }
